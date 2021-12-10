8fa1abf9f914d013ab35e04b3c0bcd5ae9fce26c
Francesco Martini <wrCisco@users.noreply.github.com>
2021-12-10 07:07:27 +0100

    Add solutions day 10

---
+++ day10/day10.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+
+from functools import reduce
+
+
+def run():
+    with open('input.txt') as fh:
+        lines = fh.read().splitlines()
+
+    matches = { '(': ')', '[': ']', '{': '}', '<': '>' }
+    points1 = { ')': 3, ']': 57, '}': 1197, '>': 25137 }
+    points2 = { ')': 1, ']': 2, '}': 3, '>': 4 }
+    score1 = 0
+    score2 = []
+    for line in lines:
+        opened = []
+        for char in line:
+            if char in points1:
+                if opened:  # there are no initial closing braces in input
+                    if char != matches[opened[-1]]:
+                        score1 += points1[char]
+                        break
+                    opened.pop()
+            else:
+                opened.append(char)
+        else:
+            autocomplete = [matches[c] for c in reversed(opened)]
+            score2.append(reduce(lambda x, y: x * 5 + points2[y], autocomplete, 0))
+    print(score1)  # first answer
+    print(sorted(score2)[len(score2) // 2])  # second answer
+
+
+if __name__ == '__main__':
+    run()
